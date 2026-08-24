594087d3f78cf116ab7f717d2cb1d2b8f2a6433f
madsj20 <mads98jensen@gmail.com>
2026-08-24 12:04:44 +0200

    Added Spring Boot and dependencies
    Added dependencies:
    H2 Database
    Spring Web
    Validation
    Spring Data JPA
    HTTP Client

--- README.md
+++
@@ -1,2 +0,0 @@
-# kodetest-spring-boot (Mads Jensen)
-
---
+++ src/test/java/dk/madsjensen/bankaccount/BankAccountApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.madsjensen.bankaccount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAccountApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
